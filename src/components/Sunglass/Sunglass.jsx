8af111591dbc44202e177cce765a2bf6da66e3b9
Shafkatahmad <shafkataahmad@gmail.com>
2025-08-22 22:48:09 +0600

    es6 module and import default

---
+++ src/components/Sunglass/Sunglass.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Sunglass.css'
+import Watch from '../Watch/Watch';
+import { add, multiply, divideTheFirstNumberByTheSecondNumber as divide } from '../../utils/Calculate';
+// import add from '../../utils/Calculate';
+
+const Sunglass = () => {
+  const first = 55;
+  const second = 199;
+  const sum = add(first, second);
+  const mul = multiply(first, second);
+  const vaag = divide(first, second)
+  return (
+    <div>
+      <Watch></Watch>
+    </div>
+  );
+};
+
+export default Sunglass;
---
+++ src/utils/Calculate.js
@@ -0,0 +1,15 @@
+const add = (first, second) => {
+  return first + second;
+}
+
+const multiply = (first, second) => {
+  return first * second;
+}
+
+const divideTheFirstNumberByTheSecondNumber = (first, second) => {
+  return first / second;
+}
+
+// export default add;
+// const calculate = {add: add};
+export {add, multiply, divideTheFirstNumberByTheSecondNumber}
